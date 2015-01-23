caf6134fed5dd0162fc8d6b691da582c56ee823a
hyunsikki <hyunsikki@gmail.com>
2015-01-23 13:49:11 +0900

    todo 예제 commit

---
+++ StudyAngularJs/js/todoController.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+function todoController($scope, $http)
+{
+	$http.get('sample/todo.json').success(function(data)
+	{
+		$scope.todos = data;
+	});
+
+	$scope.addTodo = function()
+	{
+		if ($scope.newTodo)
+		{
+			$scope.todos.push(
+			{
+				"checked" : "",
+				"work" : $scope.newTodo
+			});
+			
+			$scope.newTodo = null;
+		}
+		else
+		{
+			alert('할일을 입력해주세요!');
+		}
+	};
+}
